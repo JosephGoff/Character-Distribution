8f06b2e923c27286d2e5b8ad086876d5c86f96a6
Joseph Goff <40038953+JosephGoff@users.noreply.github.com>
2018-10-11 22:24:39 -0400

    Updated from Brython Server: 10/11/2018 10:24:39 PM

--- distribution.py
+++ distribution.py
@@ -36,3 +36,31 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
+userinput = input("Please enter a string of text (the bigger the better): ")
+print("The distribution of characters in " + '"' + userinput + '"'+ " is:")
+userinput = userinput.lower()
+
+characters = {'a':'','b':'','c':'','d':'','e':'','f':'','g':'','h':'','i':'','j':'','k':'','l':'','m':'','n':'','o':'','p':'','q':'','r':'','s':'','t':'','u':'','v':'','w':'','x':'','y':'','z':''}
+inputlist = []
+
+for a in userinput:
+    if a!= " " and a != "," and a != "?" and a != "!" and a != "." and a != "'" and a != ";" and a != ":":
+        characters[a]+=a
+
+"""
+for a in characters[a]:
+    if a == len(userinput):
+        print(characters[a])
+characters.sort(lambda x,y: cmp(len(x), len(y))) 
+"""
+charlist = []
+for q in characters:
+    if len(characters[q]) > 0:
+        charlist.append(characters[q])
+    
+charlist.sort()
+
+for e in range(len(userinput),0,-1):
+    for k in charlist:
+        if len(k) == e:
+            print(k)
